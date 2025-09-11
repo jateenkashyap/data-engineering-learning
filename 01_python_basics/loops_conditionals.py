f170266e8ddbaf0a7690d57af268a950684abafe
Jateen Kashyap <jatin99317@gmail.com>
2025-09-11 12:46:37 +0530

    Added loops_conditionals.py example

---
+++ 01_python_basics/loops_conditionals.py
@@ -0,0 +1,13 @@
+# List of skills
+skills = ["Python", "SQL", "ETL", "Data Modeling"]
+
+# Loop through the list
+for skill in skills:
+    print("Learning:", skill)
+
+# Conditional check
+age = 28
+if age > 25:
+    print("You have enough maturity to start data engineering seriously!")
+else:
+    print("Focus on building strong foundations first.")
